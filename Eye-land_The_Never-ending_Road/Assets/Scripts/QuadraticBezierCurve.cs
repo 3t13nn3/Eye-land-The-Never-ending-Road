1df4331333300f9ffba03d41321ed3e1182bb1c1
Etienne <etiennepenault1997@gmail.com>
2023-01-19 23:48:49 +0100

    Fixed bezier to smooth path
    Add some thikness next

--- Eye-land_The_Never-ending_Road/Assets/Scripts/QuadraticBezierCurve.cs
+++ Eye-land_The_Never-ending_Road/Assets/Scripts/QuadraticBezierCurve.cs
@@ -9,10 +9,15 @@ public class QuadraticBezierCurve : MonoBehaviour {
     
     public float _width = 10;
     public int _resolution = 50;
+    public float _thickness = 10;
 
     private MeshFilter _filter;
     private MeshRenderer _renderer;
     private Mesh _mesh;
+
+    public Vector3[] _vertices;
+    public Vector2[] _uv;
+    public int[] _triangles;
 
     // Constructor // Is destructor implied in C# ???
     public void SetPoints(Vector3[] points) {
@@ -20,7 +25,7 @@ public class QuadraticBezierCurve : MonoBehaviour {
     }
 
     // Calculate a point along t value with the quadratic Bezier formula
-    public Vector3 CalculateBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3,float t) {
+    public Vector3 CalculateBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
         float u = 1 - t;
         float tt = t * t;
         float uu = u * u;
@@ -36,8 +41,8 @@ public class QuadraticBezierCurve : MonoBehaviour {
     }
 
     // Derive at a given t to know the direction
-    public Vector3 CalculateBezierDirection(float t) {
-        Vector3 d = -3 * this._points[0] * (1 - t) * (1 - t) + 3 * this._points[1] * (1 - t) * (1 - t) - 6 * this._points[1] * t * (1 - t) + 6 * this._points[2] * t * (1 - t) - 3 * this._points[2] * t * t + 3 * this._points[3] * t * t;
+    public Vector3 CalculateBezierDirection(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+        Vector3 d = -3 * p0 * (1 - t) * (1 - t) + 3 * p1 * (1 - t) * (1 - t) - 6 * p1 * t * (1 - t) + 6 * p2 * t * (1 - t) - 3 * p2 * t * t + 3 * p3 * t * t;
         return d.normalized;
     }
 
@@ -50,7 +55,7 @@ public class QuadraticBezierCurve : MonoBehaviour {
         for (int i = 0; i < _resolution; i++) {
             float t = (float)i / (_resolution - 1);
             Vector3 point = CalculateBezierPoint(this._points[0], this._points[1], this._points[2], this._points[3], t);
-            Vector3 direction = CalculateBezierDirection(t);
+            Vector3 direction = CalculateBezierDirection(this._points[0], this._points[1], this._points[2], this._points[3], t);
             Vector3 normal = Vector3.Cross(direction, Vector3.up).normalized;
             // Debug.Log(point);
             // Debug.Log(direction);
@@ -79,14 +84,24 @@ public class QuadraticBezierCurve : MonoBehaviour {
         _mesh.vertices = vertices;
         _mesh.uv = uv;
         _mesh.triangles = triangles;
+
+        _vertices = vertices;
+        _uv = uv;
+        _triangles = triangles;
+
+        _renderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         
     }
 
     // EZ Debug with controls points
     public void OnDrawGizmos() {
-        Gizmos.color = Color.yellow;
+        
         for (int i = 0; i < this._points.Length; i++) {
-            Gizmos.DrawSphere(this._points[i], 0.1f);
+            Gizmos.color = Color.yellow;
+            if (i == 1 ||i == 2) {
+                Gizmos.color = Color.red;
+            }
+            Gizmos.DrawSphere(this._points[i], _width/5);
         }
 
         for (int i = 0; i < this._points.Length - 1; i++) {
@@ -101,5 +116,52 @@ public class QuadraticBezierCurve : MonoBehaviour {
                 Gizmos.DrawSphere(point, 0.02f);
             }
         }
+    }
+
+
+
+    // Ca marche, mais ici je veux rendre une unique courbe de bezier quadratique qui se suit ! Pas rendre un objet de toutes les courbes. Le but seraitd'avoir une seule courbe lisse, sans espace entre les courbes.
+    public void MergeAllCurves(List<QuadraticBezierCurve> _allCurves) {
+        List<Vector3> verticesList = new List<Vector3>();
+        List<Vector2> uvList = new List<Vector2>();
+        List<int> trianglesList = new List<int>();
+
+        int currentIndex = 0;
+        for(int i = 0; i < _allCurves.Count; i++) {
+            QuadraticBezierCurve currentCurve = _allCurves[i];
+            for(int j = 0; j < currentCurve._resolution; j++) {
+                verticesList.Add(currentCurve._vertices[j * 2]);
+                verticesList.Add(currentCurve._vertices[j * 2 + 1]);
+                uvList.Add(currentCurve._uv[j * 2]);
+                uvList.Add(currentCurve._uv[j * 2 + 1]);
+
+                if(j > 0) {
+                    int currentTriangleIndex = (j - 1) * 6;
+                    int[] currentTriangles = currentCurve._triangles;
+                    trianglesList.Add(currentTriangles[currentTriangleIndex] + currentIndex);
+                    trianglesList.Add(currentTriangles[currentTriangleIndex + 1] + currentIndex);
+                    trianglesList.Add(currentTriangles[currentTriangleIndex + 2] + currentIndex);
+                    trianglesList.Add(currentTriangles[currentTriangleIndex + 3] + currentIndex);
+                    trianglesList.Add(currentTriangles[currentTriangleIndex + 4] + currentIndex);
+                    trianglesList.Add(currentTriangles[currentTriangleIndex + 5] + currentIndex);
+                }
+            }
+            currentIndex += currentCurve._resolution * 2;
+        }
+
+        Vector3[] finalVertices = verticesList.ToArray();
+        Vector2[] finalUv = uvList.ToArray();
+        int[] finalTriangles = trianglesList.ToArray();
+
+        GameObject go = new GameObject();
+        MeshFilter filter = go.AddComponent<MeshFilter>();
+        MeshRenderer renderer = go.AddComponent<MeshRenderer>();
+        Mesh finalMesh = new Mesh();
+        filter.mesh = finalMesh;
+
+        finalMesh.vertices = finalVertices;
+        finalMesh.uv = finalUv;
+        finalMesh.triangles = finalTriangles;
+        renderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
 }
--- Eye-land_The_Never-ending_Road/Assets/Scripts/RoadGeneratorBehaviour.cs
+++ Eye-land_The_Never-ending_Road/Assets/Scripts/RoadGeneratorBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 using System;
 using Random = UnityEngine.Random;
@@ -18,22 +19,15 @@ public class RoadGeneratorBehaviour : MonoBehaviour
     private List<int> _directionHistory = new List<int>();
 
     private List<QuadraticBezierCurve> _allCurves = new List<QuadraticBezierCurve>();
+    public Vector3[] _allPoints;
 
     // Based on previous tile. Generate a new tile to append and continue the road
     void GenerateNextTile() {
 
-        // choosing direction
         float xOffset = this._roadTiles[this._roadTiles.Count - 1].transform.position.x;
         float zOffset = this._roadTiles[this._roadTiles.Count - 1].transform.position.z;
 
-        int direction = 0;
-        while(true) {
-            direction = Random.Range(1, 4); 
-            if (Math.Abs(direction - this._directionHistory[this._directionHistory.Count - 1]) != (int)Direction.NORTH) {
-            Debug.Log(direction);
-                break;
-            }
-        }
+        int direction = this._directionHistory[this._directionHistory.Count - 1];
 
         if (direction == (int)Direction.WEST) {
             xOffset -= this._tileDimension.x;
@@ -43,7 +37,17 @@ public class RoadGeneratorBehaviour : MonoBehaviour
             xOffset += this._tileDimension.x;
         }
 
-        this._directionHistory.Add(direction);
+        // choosing direction for next tile
+
+        int nextDirection = 0;
+        while(true) {
+            nextDirection = Random.Range(1, 4); 
+            if (Math.Abs(nextDirection - this._directionHistory[this._directionHistory.Count - 1]) != (int)Direction.NORTH) {
+                // Debug.Log(direction);
+                break;
+            }
+        }
+        this._directionHistory.Add(nextDirection);
         this._tile.transform.position = new Vector3(xOffset, 0.5f, zOffset);
         this._tile.transform.localScale = new Vector3 (this._tileDimension.x, 0.1f, this._tileDimension.y);
         Instantiate(this._tile);
@@ -54,34 +58,39 @@ public class RoadGeneratorBehaviour : MonoBehaviour
     void GenerateRoad(int indexOfTile) {
         Vector3[] points = new Vector3[4];
 
-        // Handling directions in function of the previous tile
-        GameObject previousTile = this._roadTiles[indexOfTile];
+        GameObject currentTile = this._roadTiles[indexOfTile];
 
         points[0] = this._allCurves[this._allCurves.Count - 1]._points[3];
-
+        
 
         for (int i = 1; i < 4; i++)
         {
             points[i] = new Vector3(
-                previousTile.transform.position.x + Random.Range(-(this._tileDimension.x/3), (this._tileDimension.x/3)),
+                currentTile.transform.position.x + Random.Range(-(this._tileDimension.x/3), (this._tileDimension.x/3)),
                 0.555f, 
-                previousTile.transform.position.z + Random.Range(-(this._tileDimension.y/3), (this._tileDimension.y/3))
+                currentTile.transform.position.z + Random.Range(-(this._tileDimension.y/3), (this._tileDimension.y/3))
             ); 
         }
 
         // Not ending proprely
-        if(indexOfTile < this._roadTiles.Count - 1) {
+        {
             int direction = this._directionHistory[indexOfTile];
             if (direction == (int)Direction.WEST) {
-                points[3].x = this._roadTiles[indexOfTile+1].transform.position.x + this._tileDimension.x/2;
+                points[3].x = this._roadTiles[indexOfTile].transform.position.x - this._tileDimension.x/2;
             } else if (direction == (int)Direction.NORTH) {
-                points[3].z = this._roadTiles[indexOfTile+1].transform.position.z + this._tileDimension.y/2;
+                points[3].z = this._roadTiles[indexOfTile].transform.position.z + this._tileDimension.y/2;
             } else if (direction == (int)Direction.EAST) {
-                points[3].x = this._roadTiles[indexOfTile+1].transform.position.x + this._tileDimension.x/2;
+                points[3].x = this._roadTiles[indexOfTile].transform.position.x + this._tileDimension.x/2;
             }
         }
         
-
+        // From the article: Force first control point to be aligned with the last one
+        Vector3 t = (this._allCurves[this._allCurves.Count - 1]._points[2] - points[0]);
+        // Scaling to avoid exiting tiles
+        t.x /= 5;
+        t.z /= 5;
+        // Then applying the transformation
+        points[1] = points[0] - t;
 
         QuadraticBezierCurve c = gameObject.AddComponent<QuadraticBezierCurve>();
         c.SetPoints(points);
@@ -91,7 +100,8 @@ public class RoadGeneratorBehaviour : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        
         // First tile is always the same, straight
         {
             this._tile.transform.position = new Vector3(0f, 0.5f, 0f);
@@ -110,16 +120,22 @@ public class RoadGeneratorBehaviour : MonoBehaviour
             );
             this._allCurves.Add(c);
             c.GenerateBezierRoad();
-        } 
+        }
 
-   
         for (int i = 1; i < 100; i++)
         {
-           GenerateNextTile();
-           GenerateRoad(i);
+            GenerateNextTile();
+            GenerateRoad(i);
         }
-        
+
+        /*
+        // Generating all the road as one object
+        QuadraticBezierCurve ccc = gameObject.AddComponent<QuadraticBezierCurve>();
+        ccc.MergeAllCurves(this._allCurves);
+        */
     }
+
+
 
     // Update is called once per frame
     void Update()
