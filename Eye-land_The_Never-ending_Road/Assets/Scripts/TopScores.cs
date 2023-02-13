047872f8e3c8e30be72e6b5d82d3f0f2bc1d47a7
Nada <nada@MacBook-Pro-de-Nada.local>
2023-02-14 00:38:56 +0100

    Add score scene

---
+++ Eye-land_The_Never-ending_Road/Assets/Scripts/TopScores.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+public class TopScores : MonoBehaviour
+{
+    private Transform container;
+    private Transform template;
+
+    public void Awake()
+    {
+        
+        container = GameObject.FindWithTag("container").transform;
+        template = GameObject.FindWithTag("template").transform;
+        template.gameObject.SetActive(false);
+
+
+        float templateHeight = 33;
+
+        List<playersScore> l =  EndGameManager.LoadFromJson();
+        l.Sort((x, y) => y.playerScore.CompareTo(x.playerScore));
+        int nbr = l.Count;
+        for (int i = 0; i < nbr; i++)
+        {
+            Transform entryTransform = Instantiate(template, container);
+            RectTransform rectTransform = entryTransform.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(0, -templateHeight * (i));
+            entryTransform.gameObject.SetActive(true);
+            entryTransform.Find("playerName").GetComponent<TextMeshProUGUI>().text = l[i].playerName;
+            entryTransform.Find("playerScore").GetComponent<TextMeshProUGUI>().text = l[i].playerScore + "";
+            entryTransform.Find("background").gameObject.SetActive((i+1) % 2 ==1);
+          
+            
+            
+            
+        }
+    }
+}
